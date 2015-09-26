c4514067b60a36b83c14a33189bde71b96e26441
Fengda HUANG <h@phodal.com>
2015-09-26 13:28:30 +0800

    add infinite scroll

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -61,7 +61,8 @@ angular.module('starter.controllers', [])
   })
 
   .controller('BlogCtrl', function ($scope, Blog) {
-    $scope.blogs = {};
+    $scope.blogs = null;
+    $scope.blogOffset = 0;
     //
     $scope.doRefresh = function () {
       Blog.async('app').then(function (results) {
@@ -75,6 +76,15 @@ angular.module('starter.controllers', [])
     Blog.async('https://www.phodal.com/api/v1/app/?format=json').then(function (results) {
       $scope.blogs = results.objects;
     });
+
+    $scope.loadMore = function() {
+      $scope.blogOffset = $scope.blogOffset + 1;
+      Blog.async('https://www.phodal.com/api/v1/app/?limit=10&offset='+ $scope.blogOffset * 20 +  '&format=json').then(function (results) {
+        //$scope.blogs.push(results.objects);
+        Array.prototype.push.apply($scope.blogs, results.objects);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      })
+    };
   })
 
   .controller('BlogDetailCtrl', function ($scope, $stateParams, $sanitize, $sce, Blog) {
