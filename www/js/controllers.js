650ad9885a751493baded06d841d31088f97e458
Fengda HUANG <h@phodal.com>
2015-09-26 13:33:14 +0800

    Remove unused code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -79,7 +79,6 @@ angular.module('starter.controllers', [])
     $scope.loadMore = function() {
       $scope.blogOffset = $scope.blogOffset + 1;
       Blog.async('https://www.phodal.com/api/v1/app/?limit=10&offset='+ $scope.blogOffset * 20 +  '&format=json').then(function (results) {
-        //$scope.blogs.push(results.objects);
         Array.prototype.push.apply($scope.blogs, results.objects);
         $scope.$broadcast('scroll.infiniteScrollComplete');
       })
