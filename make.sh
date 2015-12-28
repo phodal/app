b1e646a09943aa93eba0b80b22beab6f8a05b58c
Fengda HUANG <h@phodal.com>
2015-12-28 23:27:27 +0800

    upgrade test script

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 cordova build --release android
 rm ~/learing/phodalapp/platforms/android/build/outputs/apk/blog.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/learing/phodalapp/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
-~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/learing/phodalapp/platforms/android/build/outputs/apk/blog.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/learing/phodalapp/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk phodal
+~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ~/learing/phodalapp/platforms/android/build/outputs/apk/blog.apk
